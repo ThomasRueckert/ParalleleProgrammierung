d620d1d46a380c686898bf852d2135ad2f27b776
Thomas Rueckert <rutho@caeroser.hrz.tu-chemnitz.de>
2015-05-15 10:09:04 +0200

    ue4

--- Übung/Uebung4/hostname.c
+++ Übung/Uebung4/hostname.c
@@ -2,6 +2,22 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * this is a little mpi and chich how-to
+ *
+ * ssh chiclogin1.informatik.tu-chemnitz.de
+ *
+ * nur einmalig:
+ * module initadd comp/gcc/422
+ * module initadd mpi/openmpi/gcc422
+ *
+ * programm aus dem afs in die aktuelle directory kopieren
+ * (/afs/tu-chemnitz.de/home/urz/[anfangsbuchstabe-vom-kürzel]/[urz-kürzel]/...)
+ *
+ * compilieren: mpicc programm.c -o programmname
+ * anfordern von Knoten: qsub -I -l nodes=X:ppn=1,walltime=00:20:00 -q short
+ * starten: mpirun -np X ./programmname
+ */
 int main(int argc, char** argv)
 {
     MPI_Init(&argc,&argv);
