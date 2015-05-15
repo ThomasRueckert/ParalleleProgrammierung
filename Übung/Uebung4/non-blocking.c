b61eb70c64b4a08776a083002e23627a52e1cc87
ThomasRueckert <thomas.rueckert@s2010.tu-chemnitz.de>
2015-05-15 10:48:57 +0200

    ue4

---
+++ Übung/Uebung4/non-blocking.c
@@ -0,0 +1,35 @@
+
+#include <mpi.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc,&argv);
+    int rank, comm_size;
+    char hostname[256];
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
+    char* hostnames;
+    if(rank == 0) {
+        hostnames = (char*) malloc(256*comm_size);
+        MPI_recv();
+    } else {
+        MPI_send();
+    }
+
+    MPI_Gather(hostname, 256, MPI_CHAR, hostnames, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        int i;
+        for (i = 0; i < comm_size; i++) {
+            printf("process %d: host %s\n", i, &hostnames[i*256]);
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
